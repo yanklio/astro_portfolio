b682960c35806bc465fa873390dd6c243c9e2587
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2025-12-09 02:43:33 +0100

    ui: add pixeled icons and stylized the themeIcon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
+  integrations: [icon()],
 });
