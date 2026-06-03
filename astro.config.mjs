1ed7514f5d81450a4fa2f4ba84165857ce4cba53
yanklio <y.ustinov2004@gmail.com>
2026-06-03 16:24:19 +0200

    tabler icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,26 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://yanklio.netlify.app",
-  integrations: [icon(), sitemap()],
+  integrations: [
+    icon({
+      include: {
+        tabler: [
+          "arrow-left",
+          "arrow-right",
+          "article",
+          "ban",
+          "brand-github",
+          "brand-linkedin",
+          "home",
+          "link",
+          "menu-2",
+          "moon",
+          "rss",
+          "sun",
+          "x",
+        ],
+      },
+    }),
+    sitemap(),
+  ],
 });
